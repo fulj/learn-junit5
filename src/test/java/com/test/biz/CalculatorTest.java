2b5550d3ea31b5b228aa6f67f25289d0f5d9b59e
傅良军 <wb-flj593682@alibaba-inc.com>
2019-10-16 19:48:43 +0800

    second commit

--- src/test/java/com/test/biz/CalculatorTest.java
+++ src/test/java/com/test/biz/CalculatorTest.java
@@ -5,8 +5,9 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
-
+    private final Calculator calculator = new Calculator();
     @Test
-    void add() {
+    void addition() {
+        assertEquals(2, calculator.add(1, 1));
     }
 }

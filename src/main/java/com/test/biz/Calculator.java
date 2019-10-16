56af90dd91c8114e045f4b921c46d36d258e62e6
傅良军 <wb-flj593682@alibaba-inc.com>
2019-10-16 19:22:25 +0800

    first commit

---
+++ src/main/java/com/test/biz/Calculator.java
@@ -0,0 +1,10 @@
+package com.test.biz;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+}
+
---
+++ src/test/java/com/test/biz/CalculatorTest.java
@@ -0,0 +1,12 @@
+package com.test.biz;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void add() {
+    }
+}
